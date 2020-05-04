4edd8d3121a6208e7f25e2dc1aa5406886837378
Syntax <47469072+NotSyntax@users.noreply.github.com>
2020-05-04 13:37:47 -0500

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,22 @@
+var _0x5c98 = [
+    'color:\x20#f0db4f;\x20font-size:\x202vmin;\x20font-family:\x20\x22Montserrat\x22,\x20sans-serif;',
+    'color:\x20#f0db4f;\x20font-size:\x202vmax;\x20font-family:\x20\x22Montserrat\x22,\x20sans-serif;',
+    'log',
+    '%cI\x20have\x20the\x20code\x20to\x20the\x20website\x20on\x20GitHub!\x20You\x20can\x20see\x20the\x20entirety\x20of\x20the\x20code\x20at\x20https://github.com/NotSyntax/portfolio',
+    '%cHey!'
+];
+(function (_0x2fd965, _0x5c98c0) {
+    var _0x3a80e4 = function (_0x4ce6d2) {
+        while (--_0x4ce6d2) {
+            _0x2fd965['push'](_0x2fd965['shift']());
+        }
+    };
+    _0x3a80e4(++_0x5c98c0);
+}(_0x5c98, 0x12d));
+var _0x3a80 = function (_0x2fd965, _0x5c98c0) {
+    _0x2fd965 = _0x2fd965 - 0x0;
+    var _0x3a80e4 = _0x5c98[_0x2fd965];
+    return _0x3a80e4;
+};
+console['log'](_0x3a80('0x3'), _0x3a80('0x0'));
+console[_0x3a80('0x1')](_0x3a80('0x2'), _0x3a80('0x4'));
